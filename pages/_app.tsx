23c8f1e661cd701970c1b62f7593e6b38cc130c3
Rey Mooy <gdrrey@gmail.com>
2022-11-13 21:57:32 +0800

    add font & layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+import { ReactElement } from 'react'
+import { NextPage } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const roboto = Inter({
+  weight: '400',
+  subsets: ['latin']
+})
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactElement
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return (
+    <main className={`${roboto.className} h-screen`}>
+      {getLayout(<Component {...pageProps} />)}
+    </main>
+  )
 }
