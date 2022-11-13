582af6b21a7ca948040a363dfcb60178518f2800
Rey Mooy <gdrrey@gmail.com>
2022-11-13 21:56:37 +0800

    add icons

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
-
+import { Home, MessageSquare, Settings} from 'react-feather'
 export default function SideBar() {
   return (
-    <div className='w-[300px] h-full bg-red-400'>SideBar</div>
+    <aside className='w-[300px] h-full bg-red-400'>
+      <div className='flex justify-center items-center p-4'>
+        <h1 className='text-xl bold'>Futuro</h1>
+      </div>
+      <nav className='flex flex-col items-center px-3'>
+        <Link href='/' className='block w-full rounded-md flex items-center gap-2 m-3 px-3 hover:text-green-500'>
+            <Home/>
+            <h2>Home</h2>
+        </Link>
+        <Link href='/messages' className='w-full rounded-md flex items-center gap-2 m-3 px-3 hover:text-green-500'>
+          <MessageSquare/>
+          <h2>Messages</h2>
+        </Link>
+        <Link href='/settings' className='w-full rounded-md flex items-center gap-2 m-3 px-3 hover:text-green-500'>
+          <Settings/>
+          <h2>Settings</h2>
+        </Link>
+      </nav>
+    </aside>
   )
 }
